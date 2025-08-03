3095a36ca8504efb7a0ff3114a2c4e4a1d83a17c
Aritra-kayal <aritra9434853448@gmail.com>
2025-08-03 22:10:11 +0530

    ned code

---
+++ C-programming/typConversion2.c
@@ -0,0 +1,18 @@
+// implicit conversion
+#include <stdio.h>
+int main()
+{
+    int a = 1.999999;
+    float b = 1.5;
+    printf("%d\n", b);
+    printf("%d\n", a);
+    printf("%f\n", a);
+    printf("%d\n", 1);
+    printf("%f\n", 1.500000);
+    printf("%d\n", 1.500000);
+    printf("%f\n", 1.35);
+    printf("%d\n", 1.35);
+    printf("%d\n", 1.3600);
+    printf("%f\n", 1);
+    return 0;
+}
