e66e1ce1b23977bb628466daaa3944d904b797d4
Aritra-kayal <aritra9434853448@gmail.com>
2025-08-15 11:40:13 +0530

    new code

---
+++ C-programming/relationalOperators.c
@@ -0,0 +1,26 @@
+// relational operators
+#include <stdio.h>
+int main()
+{
+    // equal to
+    printf("%d\n", 4 == 4);
+    printf("%d\n", 4 == 3);
+    // not equal to
+    printf("%d\n", 4 != 4);
+    printf("%d\n", 4 != 3);
+    // greater than
+    printf("%d\n", 4 > 3);
+    printf("%d\n", 3 > 4);
+    printf("%d\n", 4 > 4);
+    // greater than or equal to
+    printf("%d\n", 4 >= 4);
+    printf("%d\n", 4 >= 3);
+    // less than
+    printf("%d\n", 4 < 3);
+    printf("%d\n", 3 < 4);
+    printf("%d\n", 4 < 4);
+    // less than or equal to
+    printf("%d\n", 4 <= 4);
+    printf("%d\n", 4 <= 3);
+    return 0;
+}
